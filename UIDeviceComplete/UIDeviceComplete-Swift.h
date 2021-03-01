513c15bc1c728636428bc9b83155e6b1852fa028
y4gamilight <thanhlt.ios.36@gmail.com>
2021-03-01 23:31:40 +0700

    -Update call method to get common device

--- UIDeviceComplete/UIDeviceComplete-Swift.h
+++ UIDeviceComplete/UIDeviceComplete-Swift.h
@@ -5,9 +5,14 @@
 //  Created by Le Tan Thanh on 3/1/21.
 //  Copyright © 2021 Nicholas Maccharoli. All rights reserved.
 //
+#import <UIKit/UIKit.h>
 
 #ifndef UIDeviceComplete_Swift_h
 #define UIDeviceComplete_Swift_h
  
 @class Y4gDeviceMain;
+@protocol Y4gDeviceMain <NSObject>
+- (NSString *)getDeviceModel;
+
+@end
 #endif /* UIDeviceComplete_Swift_h */
