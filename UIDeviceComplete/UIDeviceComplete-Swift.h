8c1f1a230d660d5d16c22cf7e4e28566ed4be29e
y4gamilight <thanhlt.ios.36@gmail.com>
2021-03-01 21:59:25 +0700

    -Update file Swift Header

---
+++ UIDeviceComplete/UIDeviceComplete-Swift.h
@@ -0,0 +1,13 @@
+//
+//  UIDeviceComplete-Swift.h
+//  UIDeviceComplete
+//
+//  Created by Le Tan Thanh on 3/1/21.
+//  Copyright © 2021 Nicholas Maccharoli. All rights reserved.
+//
+
+#ifndef UIDeviceComplete_Swift_h
+#define UIDeviceComplete_Swift_h
+
+
+#endif /* UIDeviceComplete_Swift_h */
